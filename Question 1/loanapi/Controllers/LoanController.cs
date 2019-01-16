ec0060699fe481fe78a17bbf1be9bea8acabc300
aphaphon <pui.aphaphon@gmail.com>
2019-01-16 11:02:21 +0700

    Implement loan 1st

--- Question 1/loanapi/Controllers/LoanController.cs
+++ Question 1/loanapi/Controllers/LoanController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using loanapi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace loanapi.Controllers
@@ -10,6 +11,22 @@ namespace loanapi.Controllers
     [ApiController]
     public class LoanController : ControllerBase
     {
-        
+        private static List<Loan> loans = new List<Loan>();
+        private static int Count = 1;
+
+        [HttpPost]
+        public void Post([FromBody] Loan loan)
+        {
+            loan.CountYear = $"ปีที่{Count++}:";
+            loan.InterestRate = loan.Balance * 12 / 100;
+            loan.Payment = loan.Balance + loan.InterestRate;
+            loans.Add(loan);
+        }
+
+         [HttpGet]
+        public IEnumerable<Loan> Get()
+        {
+            return loans;
+        }
     }
 }
---
+++ Question 1/loanapi/Models/Loan.cs
@@ -0,0 +1,10 @@
+namespace loanapi.Models
+{
+    public class Loan
+    {
+        public string CountYear { get; set; }
+        public double Balance { get; set; }
+        public double InterestRate { get; set; }
+        public double Payment { get; set; }
+    }
+}
